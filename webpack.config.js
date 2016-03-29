918112e2ec578ffecb4683c9ca94dc3123a2bfbe
Miguel Michelson <miguelmichelson@gmail.com>
2016-03-29 02:13:58 -0300

    dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ module.exports = {
   app: ['webpack/hot/dev-server', './javascripts/entry.js'],
 },
 output: {
-  path: './public/built',
+  path: './dist/built',
   filename: 'bundle.js',
   publicPath: 'http://localhost:8080/built/'
 },
 devServer: {
-  contentBase: './public',
+  contentBase: './dist',
   publicPath: 'http://localhost:8080/built/'
 },
 module: {
